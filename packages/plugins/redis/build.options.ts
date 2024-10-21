f053cc6179e467900f03647df99e872553acddd4
Wojtek Gorzawski <wojtek@c4w.site>
2024-10-21 18:21:48 +0200

    feat(redis): fixed build config

--- packages/plugins/redis/build.options.ts
+++ packages/plugins/redis/build.options.ts
@@ -3,10 +3,5 @@ import { BaseBuildOptions } from '../../shared/build.options';
 
 export default defineBuildConfig({
   ...BaseBuildOptions,
-  rollup: {
-    ...BaseBuildOptions.rollup,
-    esbuild: {
-      exclude: ['ioredis']
-    }
-  }
+  externals: ['ioredis']
 })
