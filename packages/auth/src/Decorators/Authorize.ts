6b5c34a5db39348b057c79f96abc7ec2af7b6a11
Attila Orosz <atttila.orosz@gmail.com>
2025-02-24 20:33:32 +0100

    feat(auth): authorization middleware (#17)

---
+++ packages/auth/src/Decorators/Authorize.ts
@@ -0,0 +1,30 @@
+import { initializeMetadata, initializeMetadataMethod } from '@vercube/core';
+import { AuthorizationTypes } from '../Types/AuthorizationTypes';
+import { AuthorizationMiddleware } from '../Middleware/AuthorizationMiddleware';
+
+/**
+ * Authorization decorator that adds middleware to protect routes or controllers
+ * @param params Parameters for the authorization middleware
+ * @param options Optional options for the authorization middleware
+ * @returns A decorator function that adds authorization middleware to the target
+ */
+export function Authorize<T>(params: T, options?: AuthorizationTypes.MiddlewareOptions<T>): Function {
+  return function internalDecorator(target: Function, propertyName?: string) {
+    const meta = initializeMetadata((propertyName) ? target : target.prototype);
+
+    if (propertyName) {
+      initializeMetadataMethod(target, propertyName);
+    }
+
+    meta.__middlewares.push({
+      target: propertyName ?? '__global__',
+      type: 'before',
+      priority: -998,
+      middleware: AuthorizationMiddleware,
+      args: {
+        params,
+        options,
+      },
+    });
+  };
+}
