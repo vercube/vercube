fca46dafbffd9f0d0232ead1456e0e3e5146b0ef
Oskar Lebuda <oskar.lebuda@enp.pl>
2025-06-10 16:42:51 +0200

    fix(core): fix dotenv loading

--- packages/core/src/Config/Loader.ts
+++ packages/core/src/Config/Loader.ts
@@ -1,4 +1,4 @@
-import { loadConfig }  from 'c12';
+import { loadConfig, setupDotenv }  from 'c12';
 import { defu } from 'defu';
 import { ConfigTypes } from '../Types/ConfigTypes';
 import { defaultConfig } from './DefaultConfig';
@@ -16,6 +16,11 @@ export async function loadVercubeConfig(overrides?: ConfigTypes.Config): Promise
     globalRc: false,
     defaults: defaultConfig,
   });
+
+  // override dotenv
+  if (config?.config?.c12?.dotenv && typeof config?.config?.c12?.dotenv === 'object') {
+    await setupDotenv(config.config.c12.dotenv);
+  }
 
   return defu(overrides ?? {}, config?.config ?? {});
 }
