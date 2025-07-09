de4edc3e9ed1871020e502d02e9f631b78fbbcfc
Oskar Lebuda <oskar.lebuda@enp.pl>
2025-07-09 20:20:56 +0200

    feat: add new release scripts

---
+++ scripts/release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Restore all git changes
+git restore -s@ -SW  -- packages docs
+
+# Release all packages to npm registry
+pnpm release
+
+# Get the latest and pre-latest git tags
+LATEST_TAG=$(git tag --sort=-creatordate | sed -n '1p')
+PREV_TAG=$(git tag --sort=-creatordate | sed -n '2p')
+
+# Generate changelog
+pnpm changelogen --output CHANGELOG.md --from=v$PREV_TAG --to=v$LATEST_TAG
+
+# Wait for user to review and edit changelog
+echo "Press ENTER when ready to commit changes..."
+read
+
+# Commit and push changelog
+git add CHANGELOG.md && git commit -m "chore(release): v$LATEST_TAG" && git push
+
+# Sync github releases with changelog
+pnpm changelogen gh release
