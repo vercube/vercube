aa3d5a9cd6229b1851953e5fe1d0f884376e94ea
Oskar Lebuda <oskar.lebuda@enp.pl>
2025-07-28 14:18:56 +0200

    fix(schema): remove basic schemas

--- packages/schema/src/Schemas/ValidationErrorSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { BaseHttpErrorSchema } from './BaseHttpErrorSchema';
-
-export const ValidationErrorSchema: z.ZodType = z.object({
-  ...BaseHttpErrorSchema,
-  errors: z.array(z.any()).optional().openapi({
-    description: 'The errors of the validation',
-  }),
-}).openapi('ValidationError');
--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -7,10 +7,6 @@ export * from './Decorators/Schema';
 // Plugin
 export * from './Plugins/SchemaPlugin';
 
-// Schemas
-export * from './Schemas/BaseHttpErrorSchema';
-export * from './Schemas/ValidationErrorSchema';
-
 // extends ZOD schema
 extendZodWithOpenApi(z);
 
