744f694c63d5d9681322029ca5429e80603200a7
Oskar Lebuda <o.lebuda@gmail.com>
2025-08-15 21:09:16 +0000

    feat(serverless): fix playground

--- playground/src/serverless.ts
+++ playground/src/serverless.ts
@@ -1,9 +1,10 @@
 import { createApp } from '@vercube/core';
 import { toServerlessHandler } from '@vercube/serverless/aws-lambda';
 import { useContainer } from './Boot/Container';
+import { setup } from './Boot/Setup';
 import type { ServerlessHandler } from '@vercube/serverless';
 
-const app = await createApp();
+const app = await createApp({ setup });
 app.container.expand(useContainer);
 
 export const handler: ServerlessHandler = toServerlessHandler(app);
