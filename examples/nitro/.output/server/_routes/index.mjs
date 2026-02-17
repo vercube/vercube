0ef5f0c82b07545db98f16dbcfe9ceb66797d2a5
Oskar Lebuda <oskar.lebuda@enp.pl>
2026-02-17 11:05:12 +0100

    build: remove unussed files

--- examples/nitro/.output/server/_routes/index.mjs
+++
@@ -1,5 +0,0 @@
-import { r as defineHandler } from "../_libs/h3+srvx+rou3.mjs";
-var routes_default = defineHandler((event) => {
-	return "worldaaaaaaaaa";
-});
-export { routes_default as default };
