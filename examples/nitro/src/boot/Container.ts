4809f95692f9c72304cdbd954f22d179c2511250
Oskar Lebuda <oskar.lebuda@enp.pl>
2026-03-17 00:31:24 +0100

    feat(nitro): add base docs

--- examples/nitro/src/boot/Container.ts
+++
@@ -1,7 +0,0 @@
-import FooController from '@/controller/FooController';
-import { Container } from '@vercube/di';
-
-export function useContainer(container: Container): void {
-  // register controllers
-  container.bind(FooController);
-}
