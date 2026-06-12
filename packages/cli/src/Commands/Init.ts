8f4bd829536f97ebb56be8c766580837976ba369
Oskar Lebuda <oskar.lebuda@enp.pl>
2026-06-12 08:10:43 +0200

    feat: add support for aube manager

--- packages/cli/src/Commands/Init.ts
+++ packages/cli/src/Commands/Init.ts
@@ -25,6 +25,7 @@ const pms: Record<PackageManagerName, undefined> = {
   yarn: undefined,
   bun: undefined,
   deno: undefined,
+  aube: undefined,
 };
 
 const packageManagerOptions = Object.keys(pms) as PackageManagerName[];
